68b1eb890248b8a24ce0597af28630aee5ed050d
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-20 11:40:15 +0530

    ##Complete registration form interface##

--- Anguler_Client/buyAndSell/src/app/app.module.ts
+++ Anguler_Client/buyAndSell/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
