51de930b159c65c7d7711edfeea181f9cebb57a1
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-20 23:22:38 +0530

    Connect with mongoDB  database

---
+++ Anguler_Server/models/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/buyAndSellDB' , { useNewUrlParser: true } , (err)=>{
+    if (!err) { console.log('Mongodb connection success')}
+
+    else { console.log('Error in mongodb conection:' + err)}
+});
