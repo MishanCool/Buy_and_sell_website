20824818d0166dcfd77c01f5ec09e6c9f04350c1
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-22 19:50:08 +0530

    Require users.controller file and create url as /register

--- Anguler_Server/server.js
+++ Anguler_Server/server.js
@@ -2,9 +2,13 @@ require('./models/db');
 
 const express = require('express');
 
+const usersController = require('./controllers/users.controller');
+
 var app = express()
 
 
 app.listen(3002 , () => {
     console.log('Express server start at port 3002');
 });
+
+app.use('/register' , usersController);
