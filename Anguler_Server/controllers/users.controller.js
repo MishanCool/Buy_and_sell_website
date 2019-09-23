13f34153e5a987e17d9d7ec8728489880cf9c4b5
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-23 23:24:59 +0530

    Create retrive function and insert function

--- Anguler_Server/controllers/users.controller.js
+++ Anguler_Server/controllers/users.controller.js
@@ -1,8 +1,42 @@
 const express = require('express');
 var router = express.Router();
 
+var { usersView } = require('../models/users.model');
+
+// retrive function
 router.get('/' , (req,res) => {
-    res.json('sample string');
+    // res.json('sample string');
+    usersView.find((err , docs) => {
+        if(!err) { res.send(docs); }
+        else { console.log('Error in retriving users:' + err); }
+    });
+});
+
+
+router.post('/' , (req,res) => {
+    var usersSchema = new usersView ({
+        fullName: req.body.fullName,
+    
+        email: req.body.email,
+    
+        password: req.body.password,
+    
+        address: req.body.address,
+    
+        address2: req.body.address2,
+    
+        // password: req.body.email,
+    
+        city: req.body.city,
+    
+        state: req.body.state,
+    
+        zip: req.body.zip,
+    });
+    usersSchema.save((err , doc) => {
+        if(!err) { res.send(doc); }
+        else { console.log('Error in insert users:' + err); }
+    });
 });
 
 module.exports = router;
