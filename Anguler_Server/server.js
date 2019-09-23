5d759af6ea2fbf4db54eb454922b736b9edb8ccc
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-23 23:07:54 +0530

    Create bodyParser

--- Anguler_Server/server.js
+++ Anguler_Server/server.js
@@ -1,14 +1,17 @@
 require('./models/db');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const usersController = require('./controllers/users.controller');
 
 var app = express()
 
+app.use(bodyParser.json());
 
 app.listen(3002 , () => {
     console.log('Express server start at port 3002');
 });
 
 app.use('/register' , usersController);
+
