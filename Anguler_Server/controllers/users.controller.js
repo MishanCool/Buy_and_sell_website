a987364e4c3215749dec5fb90b21a3ef7229583c
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-25 23:05:46 +0530

    Create get function to retrive data from given id

--- Anguler_Server/controllers/users.controller.js
+++ Anguler_Server/controllers/users.controller.js
@@ -16,6 +16,11 @@ router.get('/' , (req,res) => {
 router.get('/:id' , (req,res) => {
     if(!ObjectId.isValid(req.params.id))
         return res.status(400).send(`No record with given id : ${req.params.id}`);
+    
+    usersView.findById(req.params.id , (err , doc) => {
+        if(!err) { res.send(doc); }
+        else { console.log('Error in retrive user : ' +err); }
+    });
 });
 
 
