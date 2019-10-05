2cbf965797e017b0aa37b35ebb41f62279bfa1f7
Mishan Madhupa <mishancool2016@gmail.com>
2019-10-05 22:36:55 +0530

    Add mongoDB colection fileds

--- Anguler_Client/buyAndSell/src/app/shared/users.model.ts
+++ Anguler_Client/buyAndSell/src/app/shared/users.model.ts
@@ -1,2 +1,22 @@
 export class Users {
+
+  // tslint:disable-next-line: variable-name
+  _id: String;
+  // tslint:disable-next-line: ban-types
+  fullName: String;
+
+  email: String;
+
+  password: String;
+
+  address: String;
+
+  address2: String;
+
+  city: String;
+
+  state: String;
+
+  zip: String;
+
 }
