5407513220b545074faf405e8c0363a4f4c08204
Mishan Madhupa <mishancool2016@gmail.com>
2019-10-08 23:39:37 +0530

    Create UsersService class

--- Anguler_Client/buyAndSell/src/app/shared/users.service.ts
+++ Anguler_Client/buyAndSell/src/app/shared/users.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { from } from 'rxjs';
+
+import { Users } from './users.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  selectedUser: Users; // users.model.ts file eke thiyana Users class eka
+  users: Users[];
+
 
   constructor() { }
 }
