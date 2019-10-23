d0d8dace405f299884713865ab49978dc93d9d8f
Mishan Madhupa <mishancool2016@gmail.com>
2019-10-23 22:29:04 +0530

    Import httpclient

--- Anguler_Client/buyAndSell/src/app/app.module.ts
+++ Anguler_Client/buyAndSell/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { NavigationComponent } from './components/navigation/navigation.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
