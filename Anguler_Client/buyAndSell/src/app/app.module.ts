619cd3fe00e9c4618573507c07b0d4de675e8c0e
Mishan Madhupa <mishancool2016@gmail.com>
2019-10-23 23:14:56 +0530

    ## Link navigation and home

--- Anguler_Client/buyAndSell/src/app/app.module.ts
+++ Anguler_Client/buyAndSell/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
